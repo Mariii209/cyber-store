974f49c7dc7b941ed33127fe10b85db0f333ed12
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-05-31 09:58:05 -0700

    Added React to the project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section>Section 1</section>
+      <section>Section 2</section>
+      <section>Section 3</section>
+      <section>Section 4</section>
+      <section>Section 5</section>
+      <section>Section 6</section>
+    </div>
+  );
+}
+
+export default App;
