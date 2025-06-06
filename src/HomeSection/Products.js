ca9b37bb4869109d810503ecdf9db10b3d6148f0
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-06-06 15:26:39 -0700

    Add Products component with images and product card layout

---
+++ src/HomeSection/Products.js
@@ -0,0 +1,141 @@
+import React from "react";
+import "./Products.css";
+import Iphone14pro from "./Iphone14pro.png";
+import AppleiPad9 from "./AppleiPad9.png";
+import GalaxyBuds from "./GalaxyBuds.png";
+import GalaxyZFold5 from "./GalaxyZFold5.png";
+
+export default function Products() {
+  return (
+    <section className="Products">
+      <div className="ProductFilters">
+        <h4 className="TabNewArrival">New Arrival</h4>
+        <h4 className="TabBestseller">Bestseller</h4>
+        <h4 className="TabFeatured">Featured Products</h4>
+      </div>
+
+      <div className="ProductGrid">
+        <div className="ProductCard">
+          <i class="fa-regular fa-heart Heart"></i>
+          <img
+            src={Iphone14pro}
+            alt="Apple iPhone 14 Pro Max 128GB Deep Purple"
+          />
+          <p className="ProductTitle">
+            Apple iPhone 14 Pro Max 128GB Deep Purple
+          </p>
+          <p className="ProductPrice">$900</p>
+          <div className="BuyButtonWrapper">
+            <a className="BuyNowButton" href="/">
+              Buy Now
+            </a>
+          </div>
+        </div>
+
+        <div className="ProductCard">
+          <i class="fa-regular fa-heart Heart"></i>
+          <img src={Iphone14pro} alt="Blackmagic Pocket Cinema Camera 6k" />
+          <p className="ProductTitle">Blackmagic Pocket Cinema Camera 6k</p>
+          <p className="ProductPrice">$2535</p>
+          <div className="BuyButtonWrapper">
+            <a className="BuyNowButton" href="/">
+              Buy Now
+            </a>
+          </div>
+        </div>
+
+        <div className="ProductCard">
+          <i class="fa-regular fa-heart Heart"></i>
+          <img
+            src={Iphone14pro}
+            alt="Apple Watch Series 9 GPS 41mm Starlight Aluminium "
+          />
+          <p className="ProductTitle">
+            Apple Watch Series 9 GPS 41mm Starlight Aluminium
+          </p>
+          <p className="ProductPrice">$399</p>
+          <div className="BuyButtonWrapper">
+            <a className="BuyNowButton" href="/">
+              Buy Now
+            </a>
+          </div>
+        </div>
+
+        <div className="ProductCard">
+          <i class="fa-regular fa-heart Heart"></i>
+          <img src={Iphone14pro} alt="AirPods Max Silver Starlight Aluminium" />
+          <p className="ProductTitle">AirPods Max Silver Starlight Aluminium</p>
+          <p className="ProductPrice">$549</p>
+          <div className="BuyButtonWrapper">
+            <a className="BuyNowButton" href="/">
+              Buy Now
+            </a>
+          </div>
+        </div>
+
+        <div className="ProductCard">
+          <i class="fa-regular fa-heart Heart"></i>
+          <img
+            src={Iphone14pro}
+            alt="Samsung Galaxy Watch6 Classic 47mm Black"
+          />
+          <p className="ProductTitle">
+            Samsung Galaxy Watch6 Classic 47mm Black
+          </p>
+          <p className="ProductPrice">$369</p>
+          <div className="BuyButtonWrapper">
+            <a className="BuyNowButton" href="/">
+              Buy Now
+            </a>
+          </div>
+        </div>
+
+        <div className="ProductCard">
+          <i class="fa-regular fa-heart Heart"></i>
+          <img
+            src={GalaxyZFold5}
+            alt="Galaxy Z Fold5 Unlocked | 256GB | Phantom Black"
+          />
+          <p className="ProductTitle">
+            Galaxy Z Fold5 Unlocked | 256GB | Phantom Black
+          </p>
+          <p className="ProductPrice">$1799</p>
+          <div className="BuyButtonWrapper">
+            <a className="BuyNowButton" href="/">
+              Buy Now
+            </a>
+          </div>
+        </div>
+
+        <div className="ProductCard">
+          <i class="fa-regular fa-heart Heart"></i>
+          <img src={GalaxyBuds} alt="Galaxy Buds FE Graphite" />
+          <p className="ProductTitle">Galaxy Buds FE Graphite</p>
+          <p className="ProductPrice">$99.99</p>
+          <div className="BuyButtonWrapper">
+            <a className="BuyNowButton" href="/">
+              Buy Now
+            </a>
+          </div>
+        </div>
+
+        <div className="ProductCard">
+          <i class="fa-regular fa-heart Heart"></i>
+          <img
+            src={AppleiPad9}
+            alt="Apple iPad 9 10.2 64GB Wi-Fi Silver (MK2L3) 2021"
+          />
+          <p className="ProductTitle">
+            Apple iPad 9 10.2" 64GB Wi-Fi Silver (MK2L3) 2021
+          </p>
+          <p className="ProductPrice">$398</p>
+          <div className="BuyButtonWrapper">
+            <a className="BuyNowButton" href="/">
+              Buy Now
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
