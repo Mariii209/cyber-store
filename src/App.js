bf7fb44258c4024e574e770b134c2e283424129b
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-06-14 22:21:48 -0700

    Add Footer component to App layout
    - Create Footer.js and Footer.css with basic branding and site info
    - Import & render <Footer /> in App.js below CyberStoreHome
    - Apply simple black background styling for visual separation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import CyberStoreHome from "./HomeSection/CyberStoreHome";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <CyberStoreHome />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div>
+        <div>
+          <h3>Cyber</h3>
+          <p>
+            We are a residential interior design firm located in Portland. Our
+            boutique-studio offers more than
+          </p>
+        </div>
+        <div>
+          <h3>
+            <h4>Services</h4>
+            <p>Bonus program</p>
+            <p>Gift cards</p>
+            <p>Credit and payment</p>
+            <p>Service contracts</p>
+            <p>Non-cash account</p>
+            <p>Payment</p>
+          </h3>
+        </div>
+        <div>
+          <h4>Assistance to the buyer</h4>
+          <p>Find an order</p>
+          <p>Term of delivery</p>
+          <p>Exchange and return of goods</p>
+          <p>Guarantee</p>
+          <p>Frequently asked questions</p>
+          <p>Terms of use of the site</p>
+        </div>
+      </div>
+      <div>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-tiktok"></i>
+        <i class="fa-brands fa-instagram"></i>
+      </div>
+    </div>
+  );
+}
