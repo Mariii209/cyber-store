85c6a30da3e4ec6973f354fbc0396e89a1adbe32
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-06-15 12:24:49 -0700

    Add footer component with content, styling, and logo asset
    - src/Footer.js: structured footer sections: about, services list, social links
    - src/Footer.css: flex layout, typography, spacing, and color rules
    - src/LogoCyber.png: footer brand logo added to component

--- src/Footer.js
+++ src/Footer.js
@@ -1,43 +1,56 @@
 import React from "react";
 import "./Footer.css";
+import LogoCyber from "./LogoCyber.png";
 
 export default function Footer() {
   return (
     <div className="Footer">
-      <div>
-        <div>
-          <h3>Cyber</h3>
+      <div className="FooterContent">
+        <section className="FooterAbout">
+          <img src={LogoCyber} alt="" />
           <p>
             We are a residential interior design firm located in Portland. Our
             boutique-studio offers more than
           </p>
-        </div>
-        <div>
-          <h3>
-            <h4>Services</h4>
-            <p>Bonus program</p>
-            <p>Gift cards</p>
-            <p>Credit and payment</p>
-            <p>Service contracts</p>
-            <p>Non-cash account</p>
-            <p>Payment</p>
-          </h3>
-        </div>
-        <div>
+        </section>
+
+        <section className="FooterServices">
+          <h4>Services</h4>
+          <ul>
+            <li>Bonus program</li>
+            <li>Gift cards</li>
+            <li>Credit and payment</li>
+            <li>Service contracts</li>
+            <li>Non-cash account</li>
+            <li>Payment</li>
+          </ul>
+        </section>
+
+        <section>
           <h4>Assistance to the buyer</h4>
-          <p>Find an order</p>
-          <p>Term of delivery</p>
-          <p>Exchange and return of goods</p>
-          <p>Guarantee</p>
-          <p>Frequently asked questions</p>
-          <p>Terms of use of the site</p>
-        </div>
+          <ul>
+            <li>Find an order</li>
+            <li>Delivery terms</li>
+            <li>Exchange and return</li>
+            <li>Guarantee</li>
+            <li>FAQs</li>
+            <li>Terms of use</li>
+          </ul>
+        </section>
       </div>
-      <div>
-        <i class="fa-brands fa-twitter"></i>
-        <i class="fa-brands fa-facebook-f"></i>
-        <i class="fa-brands fa-tiktok"></i>
-        <i class="fa-brands fa-instagram"></i>
+      <div className="FooterSocial">
+        <a href="/" aria-label="Twitter">
+          <i class="fa-brands fa-twitter"></i>
+        </a>
+        <a href="/" aria-label="Facebook">
+          <i class="fa-brands fa-facebook-f"></i>
+        </a>
+        <a href="/" aria-label="TikTok">
+          <i class="fa-brands fa-tiktok"></i>
+        </a>
+        <a href="/" aria-label="Instagram">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
       </div>
     </div>
   );
