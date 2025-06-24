318351b8f08b4c635e0acee9715f3677c69b4a77
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-06-23 18:50:56 -0700

    Create ProductPage.js

---
+++ src/HomeSection/Products Page/ProductPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ProductPage() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li> Catalog</li>
+        <li>Smartphone</li>
+      </ul>
+    </div>
+  );
+}
