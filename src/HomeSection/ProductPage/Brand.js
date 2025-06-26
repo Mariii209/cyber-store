24fc4f2378c902f7f6f6851bd4866009fe387cd8
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-06-26 14:51:33 -0700

    Add Brand component and integrate it into ProductPagePhone; style NavBar with bottom border

---
+++ src/HomeSection/ProductPage/Brand.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Brand() {
+  return (
+    <div className="Brand">
+      <h3>Brand</h3>
+      <i class="fa-solid fa-chevron-up"></i>
+      <hr />
+      <form>
+        <input type="search" name="" id="" />
+      </form>
+      <div className="BrandList">
+        <ul>
+          <li>Apple</li>
+          <li>Samsung</li>
+          <li>Google</li>
+          <li>OnePlus</li>
+          <li>Xiaomi</li>
+          <li>Oppo</li>
+          <li>Vivo</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/HomeSection/ProductPage/ProductPagePhone.js
+++ src/HomeSection/ProductPage/ProductPagePhone.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import NavBar from "../../NavBar";
+import Brand from "./Brand";
 
 export default function ProductPagePhone() {
   const { category } = useParams();
   return (
     <div>
+      <NavBar />
       <ul>
         <li>Home</li>
         <li> Catalog</li>
         <li>{category}</li>
       </ul>
+      <dic className="Content">
+        <Brand />
+      </dic>
     </div>
   );
 }
