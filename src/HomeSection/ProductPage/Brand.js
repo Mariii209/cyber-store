592fe7af916711494de3670633715a66708dcc4d
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-06-29 15:27:34 -0700

    Refactor Brand component structure and add styling
    - Updated Brand component structure for better organization
    - Added a ToggleSection wrapper for the brand filter UI
    - Moved brand list into a new section
    - Styled component in Brand.css with spacing, flexbox, and typography adjustments

--- src/HomeSection/ProductPage/Brand.js
+++ src/HomeSection/ProductPage/Brand.js
@@ -1,46 +1,49 @@
 import React from "react";
 import ToggleSection from "./ToggleSection";
+import "./Brand.css";
 
 export default function Brand() {
   return (
-    <ToggleSection title="Brand">
-      <form>
-        <i className="fa-solid fa-magnifying-glass SearchIcon"></i>
-        <input type="search" placeholder="Search brand..." />
-      </form>
+    <div className="Brand">
+      <ToggleSection title="Brand">
+        <form>
+          <i className="fa-solid fa-magnifying-glass SearchIcon"></i>
+          <input type="search" placeholder="Search brand..." />
+        </form>
 
-      <div className="BrandList">
-        <ul>
-          <li>
-            <input type="checkbox" id="apple" />
-            <label htmlFor="apple">Apple</label>
-          </li>
-          <li>
-            <input type="checkbox" id="samsung" />
-            <label htmlFor="samsung">Samsung</label>
-          </li>
-          <li>
-            <input type="checkbox" id="google" />
-            <label htmlFor="google">Google </label>
-          </li>
-          <li>
-            <input type="checkbox" id="huawei" />
-            <label htmlFor="huawei">Huawei</label>
-          </li>
-          <li>
-            <input type="checkbox" id="xiaomi" />
-            <label htmlFor="xiaomi">Xiaomi</label>
-          </li>
-          <li>
-            <input type="checkbox" id="oppo" />
-            <label htmlFor="oppo">Oppo</label>
-          </li>
-          <li>
-            <input type="checkbox" id="vivo" />
-            <label htmlFor="vivo">Vivo</label>
-          </li>
-        </ul>
-      </div>
-    </ToggleSection>
+        <div className="BrandList">
+          <ul>
+            <li>
+              <input type="checkbox" id="apple" />
+              <label htmlFor="apple">Apple</label>
+            </li>
+            <li>
+              <input type="checkbox" id="samsung" />
+              <label htmlFor="samsung">Samsung</label>
+            </li>
+            <li>
+              <input type="checkbox" id="google" />
+              <label htmlFor="google">Google </label>
+            </li>
+            <li>
+              <input type="checkbox" id="huawei" />
+              <label htmlFor="huawei">Huawei</label>
+            </li>
+            <li>
+              <input type="checkbox" id="xiaomi" />
+              <label htmlFor="xiaomi">Xiaomi</label>
+            </li>
+            <li>
+              <input type="checkbox" id="oppo" />
+              <label htmlFor="oppo">Oppo</label>
+            </li>
+            <li>
+              <input type="checkbox" id="vivo" />
+              <label htmlFor="vivo">Vivo</label>
+            </li>
+          </ul>
+        </div>
+      </ToggleSection>
+    </div>
   );
 }
