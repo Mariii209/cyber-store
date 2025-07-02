2522da287f2c1b67a3963cad65976008aeb90877
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-02 09:57:40 -0700

    Added Xiaomi 12 product data with color variants and corresponding images

---
+++ src/ProductData/Phones/Xiaomi/Xiaomi.js
@@ -0,0 +1,237 @@
+export const xiaomiPhones = [
+  {
+    id: "xiaomi-12",
+    name: "Xiaomi 12",
+    description:
+      "Flagship phone with powerful Snapdragon 8 Gen1 and compact design.",
+    colors: [
+      {
+        name: "Black",
+        image: "/images/xiaomi-12/black/front.jpg",
+        images: [
+          "/images/xiaomi-12/black/front.jpg",
+          "/images/xiaomi-12/black/back.jpg",
+        ],
+      },
+      {
+        name: "Blue",
+        image: "/images/xiaomi-12/blue/front.jpg",
+        images: [
+          "/images/xiaomi-12/blue/front.jpg",
+          "/images/xiaomi-12/blue/back.jpg",
+        ],
+      },
+      {
+        name: "Purple",
+        image: "/images/xiaomi-12/purple/front.jpg",
+        images: [
+          "/images/xiaomi-12/purple/front.jpg",
+          "/images/xiaomi-12/purple/back.jpg",
+        ],
+      },
+      {
+        name: "Green",
+        image: "/images/xiaomi-12/green/front.jpg",
+        images: [
+          "/images/xiaomi-12/green/front.jpg",
+          "/images/xiaomi-12/green/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 799,
+      },
+      {
+        size: "256GB",
+        price: 899,
+      },
+    ],
+    batteryCapacity: "4500mAh",
+    frontCamera: "32MP",
+    mainCamera: "50MP Wide + 13MP Ultra Wide + 5MP Telemacro",
+    cores: 8,
+    screenDiagonal: "6.28 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "419 ppi",
+    screenType: "AMOLED",
+    additionally: ["120W Fast Charging", "IP68", "Wireless Charging"],
+    cpu: "Snapdragon 8 Gen 1",
+  },
+  {
+    id: "xiaomi-12-pro",
+    name: "Xiaomi 12 Pro",
+    description: "Premium Xiaomi with triple 50MP camera and 120W charging.",
+    colors: [
+      {
+        name: "Gray",
+        image: "/images/xiaomi-12-pro/gray/front.jpg",
+        images: [
+          "/images/xiaomi-12-pro/gray/front.jpg",
+          "/images/xiaomi-12-pro/gray/back.jpg",
+        ],
+      },
+      {
+        name: "Purple",
+        image: "/images/xiaomi-12-pro/purple/front.jpg",
+        images: [
+          "/images/xiaomi-12-pro/purple/front.jpg",
+          "/images/xiaomi-12-pro/purple/back.jpg",
+        ],
+      },
+      {
+        name: "Blue",
+        image: "/images/xiaomi-12-pro/blue/front.jpg",
+        images: [
+          "/images/xiaomi-12-pro/blue/front.jpg",
+          "/images/xiaomi-12-pro/blue/back.jpg",
+        ],
+      },
+      {
+        name: "Green",
+        image: "/images/xiaomi-12-pro/green/front.jpg",
+        images: [
+          "/images/xiaomi-12-pro/green/front.jpg",
+          "/images/xiaomi-12-pro/green/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "256GB",
+        price: 999,
+      },
+      {
+        size: "512GB",
+        price: 1099,
+      },
+    ],
+    batteryCapacity: "4600mAh",
+    frontCamera: "32MP",
+    mainCamera: "50MP Wide + 50MP Ultra Wide + 50MP Telephoto",
+    cores: 8,
+    screenDiagonal: "6.73 inches",
+    screenResolution: "3200 x 1440 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "522 ppi",
+    screenType: "LTPO AMOLED",
+    additionally: ["120W Fast Charging", "50W Wireless Charging", "IP68"],
+    cpu: "Snapdragon 8 Gen 1",
+  },
+  {
+    id: "xiaomi-11lite-ne",
+    name: "Xiaomi 11 Lite NE",
+    description:
+      "Ultra-light phone with flagship Snapdragon 778G and slim design.",
+    colors: [
+      {
+        name: "Truffle Black",
+        image: "/images/xiaomi-11lite-ne/black/front.jpg",
+        images: [
+          "/images/xiaomi-11lite-ne/black/front.jpg",
+          "/images/xiaomi-11lite-ne/black/back.jpg",
+        ],
+      },
+      {
+        name: "Bubblegum Blue",
+        image: "/images/xiaomi-11lite-ne/blue/front.jpg",
+        images: [
+          "/images/xiaomi-11lite-ne/blue/front.jpg",
+          "/images/xiaomi-11lite-ne/blue/back.jpg",
+        ],
+      },
+      {
+        name: "Peach Pink",
+        image: "/images/xiaomi-11lite-ne/pink/front.jpg",
+        images: [
+          "/images/xiaomi-11lite-ne/pink/front.jpg",
+          "/images/xiaomi-11lite-ne/pink/back.jpg",
+        ],
+      },
+      {
+        name: "Snowflake White",
+        image: "/images/xiaomi-11lite-ne/white/front.jpg",
+        images: [
+          "/images/xiaomi-11lite-ne/white/front.jpg",
+          "/images/xiaomi-11lite-ne/white/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 329,
+      },
+      {
+        size: "256GB",
+        price: 379,
+      },
+    ],
+    batteryCapacity: "4250mAh",
+    frontCamera: "20MP",
+    mainCamera: "64MP Wide + 8MP Ultra Wide + 5MP Macro",
+    cores: 8,
+    screenDiagonal: "6.55 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "90Hz",
+    pixelDensity: "402 ppi",
+    screenType: "AMOLED",
+    additionally: ["33W Fast Charging", "Stereo Speakers", "5G"],
+    cpu: "Snapdragon 778G",
+  },
+  {
+    id: "redmi-note-11-pro-plus-5g",
+    name: "Redmi Note 11 Pro+ 5G",
+    description:
+      "Budget-friendly 5G phone with 120W fast charging and 108MP camera.",
+    colors: [
+      {
+        name: "Graphite Gray",
+        image: "/images/redmi-note-11-pro-plus/gray/front.jpg",
+        images: [
+          "/images/redmi-note-11-pro-plus/gray/front.jpg",
+          "/images/redmi-note-11-pro-plus/gray/back.jpg",
+        ],
+      },
+      {
+        name: "Star Blue",
+        image: "/images/redmi-note-11-pro-plus/blue/front.jpg",
+        images: [
+          "/images/redmi-note-11-pro-plus/blue/front.jpg",
+          "/images/redmi-note-11-pro-plus/blue/back.jpg",
+        ],
+      },
+      {
+        name: "Forest Green",
+        image: "/images/redmi-note-11-pro-plus/green/front.jpg",
+        images: [
+          "/images/redmi-note-11-pro-plus/green/front.jpg",
+          "/images/redmi-note-11-pro-plus/green/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 279,
+      },
+      {
+        size: "256GB",
+        price: 329,
+      },
+    ],
+    batteryCapacity: "4500mAh",
+    frontCamera: "16MP",
+    mainCamera: "108MP Wide + 8MP Ultra Wide + 2MP Macro",
+    cores: 8,
+    screenDiagonal: "6.67 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "395 ppi",
+    screenType: "AMOLED",
+    additionally: ["120W Fast Charging", "Stereo Speakers", "5G"],
+    cpu: "MediaTek Dimensity 920",
+  },
+];
