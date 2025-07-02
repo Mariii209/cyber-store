4ac6194cae2a1ba4fc90db03a53226a22fac2497
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-01 23:41:46 -0700

    Create iPhone 14 data file with detailed images and color variants

---
+++ src/ProductData/Phones/Apple/Iphone14.js
@@ -0,0 +1,331 @@
+export const iphone14Series = [
+  {
+    id: "iphone-14",
+    name: "iPhone 14",
+    description:
+      "The essential iPhone with solid performance and improved battery life.",
+    colors: [
+      {
+        name: "Blue",
+        image: "/images/iphone-14/blue/front.jpg",
+        images: [
+          "/images/iphone-14/blue/front.jpg",
+          "/images/iphone-14/blue/back.jpg",
+          "/images/iphone-14/blue/side.jpg",
+        ],
+      },
+      {
+        name: "Purple",
+        image: "/images/iphone-14/purple/front.jpg",
+        images: [
+          "/images/iphone-14/purple/front.jpg",
+          "/images/iphone-14/purple/back.jpg",
+          "/images/iphone-14/purple/side.jpg",
+        ],
+      },
+      {
+        name: "Midnight",
+        image: "/images/iphone-14/midnight/front.jpg",
+        images: [
+          "/images/iphone-14/midnight/front.jpg",
+          "/images/iphone-14/midnight/back.jpg",
+          "/images/iphone-14/midnight/side.jpg",
+        ],
+      },
+      {
+        name: "Starlight",
+        image: "/images/iphone-14/starlight/front.jpg",
+        images: [
+          "/images/iphone-14/starlight/front.jpg",
+          "/images/iphone-14/starlight/back.jpg",
+          "/images/iphone-14/starlight/side.jpg",
+        ],
+      },
+      {
+        name: "Red",
+        image: "/images/iphone-14/red/front.jpg",
+        images: [
+          "/images/iphone-14/red/front.jpg",
+          "/images/iphone-14/red/back.jpg",
+          "/images/iphone-14/red/side.jpg",
+        ],
+      },
+      {
+        name: "Yellow",
+        image: "/images/iphone-14/yellow/front.jpg",
+        images: [
+          "/images/iphone-14/yellow/front.jpg",
+          "/images/iphone-14/yellow/back.jpg",
+          "/images/iphone-14/yellow/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 799,
+      },
+      {
+        size: "256GB",
+        price: 899,
+      },
+      {
+        size: "512GB",
+        price: 1099,
+      },
+    ],
+    batteryCapacity: "3279mAh",
+    frontCamera: "12MP",
+    mainCamera: "12MP Wide + 12MP Ultra Wide",
+    cores: 6,
+    screenDiagonal: "6.1 inches",
+    screenResolution: "2532 x 1170 pixels",
+    refreshRate: "60Hz",
+    pixelDensity: "460 ppi",
+    screenType: "Super Retina XDR OLED",
+    additionally: [
+      "Face ID",
+      "Emergency SOS via satellite",
+      "Crash Detection",
+      "MagSafe",
+    ],
+    cpu: "A15 Bionic chip (5-core GPU)",
+  },
+  {
+    id: "iphone-14-plus",
+    name: "iPhone 14 Plus",
+    description:
+      "Big-screen iPhone with better battery and all the core features.",
+    colors: [
+      {
+        name: "Blue",
+        image: "/images/iphone-14-plus/blue/front.jpg",
+        images: [
+          "/images/iphone-14-plus/blue/front.jpg",
+          "/images/iphone-14-plus/blue/back.jpg",
+          "/images/iphone-14-plus/blue/side.jpg",
+        ],
+      },
+      {
+        name: "Purple",
+        image: "/images/iphone-14-plus/purple/front.jpg",
+        images: [
+          "/images/iphone-14-plus/purple/front.jpg",
+          "/images/iphone-14-plus/purple/back.jpg",
+          "/images/iphone-14-plus/purple/side.jpg",
+        ],
+      },
+      {
+        name: "Midnight",
+        image: "/images/iphone-14-plus/midnight/front.jpg",
+        images: [
+          "/images/iphone-14-plus/midnight/front.jpg",
+          "/images/iphone-14-plus/midnight/back.jpg",
+          "/images/iphone-14-plus/midnight/side.jpg",
+        ],
+      },
+      {
+        name: "Starlight",
+        image: "/images/iphone-14-plus/starlight/front.jpg",
+        images: [
+          "/images/iphone-14-plus/starlight/front.jpg",
+          "/images/iphone-14-plus/starlight/back.jpg",
+          "/images/iphone-14-plus/starlight/side.jpg",
+        ],
+      },
+      {
+        name: "Red",
+        image: "/images/iphone-14-plus/red/front.jpg",
+        images: [
+          "/images/iphone-14-plus/red/front.jpg",
+          "/images/iphone-14-plus/red/back.jpg",
+          "/images/iphone-14-plus/red/side.jpg",
+        ],
+      },
+      {
+        name: "Yellow",
+        image: "/images/iphone-14-plus/yellow/front.jpg",
+        images: [
+          "/images/iphone-14-plus/yellow/front.jpg",
+          "/images/iphone-14-plus/yellow/back.jpg",
+          "/images/iphone-14-plus/yellow/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 899,
+      },
+      {
+        size: "256GB",
+        price: 999,
+      },
+      {
+        size: "512GB",
+        price: 1199,
+      },
+    ],
+    batteryCapacity: "4323mAh",
+    frontCamera: "12MP",
+    mainCamera: "12MP Wide + 12MP Ultra Wide",
+    cores: 6,
+    screenDiagonal: "6.7 inches",
+    screenResolution: "2778 x 1284 pixels",
+    refreshRate: "60Hz",
+    pixelDensity: "458 ppi",
+    screenType: "Super Retina XDR OLED",
+    additionally: [
+      "Face ID",
+      "Emergency SOS via satellite",
+      "Crash Detection",
+      "MagSafe",
+    ],
+    cpu: "A15 Bionic chip (5-core GPU)",
+  },
+  {
+    id: "iphone-14-pro",
+    name: "iPhone 14 Pro",
+    description: "Powerful camera, Dynamic Island, and ProMotion display.",
+    colors: [
+      {
+        name: "Deep Purple",
+        image: "/images/iphone-14-pro/deep-purple/front.jpg",
+        images: [
+          "/images/iphone-14-pro/deep-purple/front.jpg",
+          "/images/iphone-14-pro/deep-purple/back.jpg",
+          "/images/iphone-14-pro/deep-purple/side.jpg",
+        ],
+      },
+      {
+        name: "Gold",
+        image: "/images/iphone-14-pro/gold/front.jpg",
+        images: [
+          "/images/iphone-14-pro/gold/front.jpg",
+          "/images/iphone-14-pro/gold/back.jpg",
+          "/images/iphone-14-pro/gold/side.jpg",
+        ],
+      },
+      {
+        name: "Silver",
+        image: "/images/iphone-14-pro/silver/front.jpg",
+        images: [
+          "/images/iphone-14-pro/silver/front.jpg",
+          "/images/iphone-14-pro/silver/back.jpg",
+          "/images/iphone-14-pro/silver/side.jpg",
+        ],
+      },
+      {
+        name: "Space Black",
+        image: "/images/iphone-14-pro/space-black/front.jpg",
+        images: [
+          "/images/iphone-14-pro/space-black/front.jpg",
+          "/images/iphone-14-pro/space-black/back.jpg",
+          "/images/iphone-14-pro/space-black/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 999,
+      },
+      {
+        size: "256GB",
+        price: 1099,
+      },
+      {
+        size: "512GB",
+        price: 1299,
+      },
+      {
+        size: "1TB",
+        price: 1499,
+      },
+    ],
+    batteryCapacity: "3200mAh",
+    frontCamera: "12MP",
+    mainCamera: "48MP Wide + 12MP Ultra Wide + 12MP Telephoto",
+    cores: 6,
+    screenDiagonal: "6.1 inches",
+    screenResolution: "2556 x 1179 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "460 ppi",
+    screenType: "Super Retina XDR OLED (ProMotion, Dynamic Island)",
+    additionally: ["Face ID", "Always-On Display", "ProRAW", "MagSafe"],
+    cpu: "A16 Bionic chip",
+  },
+  {
+    id: "iphone-14-pro-max",
+    name: "iPhone 14 Pro Max",
+    description:
+      "The ultimate iPhone with biggest screen and best battery life.",
+    colors: [
+      {
+        name: "Deep Purple",
+        image: "/images/iphone-14-pro-max/deep-purple/front.jpg",
+        images: [
+          "/images/iphone-14-pro-max/deep-purple/front.jpg",
+          "/images/iphone-14-pro-max/deep-purple/back.jpg",
+          "/images/iphone-14-pro-max/deep-purple/side.jpg",
+        ],
+      },
+      {
+        name: "Gold",
+        image: "/images/iphone-14-pro-max/gold/front.jpg",
+        images: [
+          "/images/iphone-14-pro-max/gold/front.jpg",
+          "/images/iphone-14-pro-max/gold/back.jpg",
+          "/images/iphone-14-pro-max/gold/side.jpg",
+        ],
+      },
+      {
+        name: "Silver",
+        image: "/images/iphone-14-pro-max/silver/front.jpg",
+        images: [
+          "/images/iphone-14-pro-max/silver/front.jpg",
+          "/images/iphone-14-pro-max/silver/back.jpg",
+          "/images/iphone-14-pro-max/silver/side.jpg",
+        ],
+      },
+      {
+        name: "Space Black",
+        image: "/images/iphone-14-pro-max/space-black/front.jpg",
+        images: [
+          "/images/iphone-14-pro-max/space-black/front.jpg",
+          "/images/iphone-14-pro-max/space-black/back.jpg",
+          "/images/iphone-14-pro-max/space-black/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 1099,
+      },
+      {
+        size: "256GB",
+        price: 1199,
+      },
+      {
+        size: "512GB",
+        price: 1399,
+      },
+      {
+        size: "1TB",
+        price: 1599,
+      },
+    ],
+    batteryCapacity: "4323mAh",
+    frontCamera: "12MP",
+    mainCamera: "48MP Wide + 12MP Ultra Wide + 12MP Telephoto",
+    cores: 6,
+    screenDiagonal: "6.7 inches",
+    screenResolution: "2796 x 1290 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "460 ppi",
+    screenType: "Super Retina XDR OLED (ProMotion, Dynamic Island)",
+    additionally: ["Face ID", "Always-On Display", "ProRAW", "MagSafe"],
+    cpu: "A16 Bionic chip",
+  },
+];
