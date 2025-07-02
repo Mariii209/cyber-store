b0270b1640e3e5cbaf4bff6c43e47d93bb0e587d
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-01 23:40:31 -0700

    Create Samsung phone data file and add Galaxy A52 5G details

---
+++ src/ProductData/Phones/Samsung/Galaxy.js
@@ -0,0 +1,346 @@
+export const samsungPhones = [
+  {
+    id: "galaxy-a52-5g",
+    name: "Samsung Galaxy A52 5G",
+    description:
+      "Affordable 5G phone with a high refresh rate display and solid performance.",
+    colors: [
+      {
+        name: "Awesome Black",
+        image: "/images/galaxy-a52/black/front.jpg",
+        images: [
+          "/images/galaxy-a52/black/front.jpg",
+          "/images/galaxy-a52/black/back.jpg",
+          "/images/galaxy-a52/black/side.jpg",
+        ],
+      },
+      {
+        name: "Awesome White",
+        image: "/images/galaxy-a52/white/front.jpg",
+        images: [
+          "/images/galaxy-a52/white/front.jpg",
+          "/images/galaxy-a52/white/back.jpg",
+          "/images/galaxy-a52/white/side.jpg",
+        ],
+      },
+      {
+        name: "Awesome Blue",
+        image: "/images/galaxy-a52/blue/front.jpg",
+        images: [
+          "/images/galaxy-a52/blue/front.jpg",
+          "/images/galaxy-a52/blue/back.jpg",
+          "/images/galaxy-a52/blue/side.jpg",
+        ],
+      },
+      {
+        name: "Awesome Violet",
+        image: "/images/galaxy-a52/violet/front.jpg",
+        images: [
+          "/images/galaxy-a52/violet/front.jpg",
+          "/images/galaxy-a52/violet/back.jpg",
+          "/images/galaxy-a52/violet/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 499,
+      },
+      {
+        size: "256GB",
+        price: 549,
+      },
+    ],
+    batteryCapacity: "4500mAh",
+    frontCamera: "32MP",
+    mainCamera: "64MP + 12MP + 5MP + 5MP",
+    cores: 8,
+    screenDiagonal: "6.5 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "407 ppi",
+    screenType: "Super AMOLED",
+    additionally: [
+      "IP67 Water Resistant",
+      "Stereo Speakers",
+      "Expandable Storage",
+    ],
+    cpu: "Snapdragon 750G",
+  },
+  {
+    id: "galaxy-s22-ultra",
+    name: "Samsung Galaxy S22 Ultra",
+    description:
+      "Flagship phone with S Pen, pro-grade cameras, and a large high-res display.",
+    colors: [
+      {
+        name: "Phantom Black",
+        image: "/images/galaxy-s22-ultra/black/front.jpg",
+        images: [
+          "/images/galaxy-s22-ultra/black/front.jpg",
+          "/images/galaxy-s22-ultra/black/back.jpg",
+          "/images/galaxy-s22-ultra/black/side.jpg",
+        ],
+      },
+      {
+        name: "Phantom White",
+        image: "/images/galaxy-s22-ultra/white/front.jpg",
+        images: [
+          "/images/galaxy-s22-ultra/white/front.jpg",
+          "/images/galaxy-s22-ultra/white/back.jpg",
+          "/images/galaxy-s22-ultra/white/side.jpg",
+        ],
+      },
+      {
+        name: "Burgundy",
+        image: "/images/galaxy-s22-ultra/burgundy/front.jpg",
+        images: [
+          "/images/galaxy-s22-ultra/burgundy/front.jpg",
+          "/images/galaxy-s22-ultra/burgundy/back.jpg",
+          "/images/galaxy-s22-ultra/burgundy/side.jpg",
+        ],
+      },
+      {
+        name: "Green",
+        image: "/images/galaxy-s22-ultra/green/front.jpg",
+        images: [
+          "/images/galaxy-s22-ultra/green/front.jpg",
+          "/images/galaxy-s22-ultra/green/back.jpg",
+          "/images/galaxy-s22-ultra/green/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 1199,
+      },
+      {
+        size: "256GB",
+        price: 1299,
+      },
+      {
+        size: "512GB",
+        price: 1399,
+      },
+    ],
+    batteryCapacity: "5000mAh",
+    frontCamera: "40MP",
+    mainCamera: "108MP + 12MP + 10MP + 10MP",
+    cores: 8,
+    screenDiagonal: "6.8 inches",
+    screenResolution: "3088 x 1440 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "500 ppi",
+    screenType: "Dynamic AMOLED 2X",
+    additionally: [
+      "S Pen",
+      "IP68 Water Resistant",
+      "Wireless DeX",
+      "Ultra-wideband",
+    ],
+    cpu: "Snapdragon 8 Gen 1",
+  },
+  {
+    id: "galaxy-a53-5g",
+    name: "Samsung Galaxy A53 5G",
+    description: "Mid-range 5G phone with AMOLED display and large battery.",
+    colors: [
+      {
+        name: "Awesome Black",
+        image: "/images/galaxy-a53/black/front.jpg",
+        images: [
+          "/images/galaxy-a53/black/front.jpg",
+          "/images/galaxy-a53/black/back.jpg",
+          "/images/galaxy-a53/black/side.jpg",
+        ],
+      },
+      {
+        name: "Awesome White",
+        image: "/images/galaxy-a53/white/front.jpg",
+        images: [
+          "/images/galaxy-a53/white/front.jpg",
+          "/images/galaxy-a53/white/back.jpg",
+          "/images/galaxy-a53/white/side.jpg",
+        ],
+      },
+      {
+        name: "Awesome Blue",
+        image: "/images/galaxy-a53/blue/front.jpg",
+        images: [
+          "/images/galaxy-a53/blue/front.jpg",
+          "/images/galaxy-a53/blue/back.jpg",
+          "/images/galaxy-a53/blue/side.jpg",
+        ],
+      },
+      {
+        name: "Awesome Peach",
+        image: "/images/galaxy-a53/peach/front.jpg",
+        images: [
+          "/images/galaxy-a53/peach/front.jpg",
+          "/images/galaxy-a53/peach/back.jpg",
+          "/images/galaxy-a53/peach/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 449,
+      },
+      {
+        size: "256GB",
+        price: 499,
+      },
+    ],
+    batteryCapacity: "5000mAh",
+    frontCamera: "32MP",
+    mainCamera: "64MP + 12MP + 5MP + 5MP",
+    cores: 8,
+    screenDiagonal: "6.5 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "405 ppi",
+    screenType: "Super AMOLED",
+    additionally: [
+      "IP67 Water Resistant",
+      "Stereo Speakers",
+      "Expandable Storage",
+    ],
+    cpu: "Exynos 1280",
+  },
+  {
+    id: "galaxy-z-flip-3",
+    name: "Samsung Galaxy Z Flip 3",
+    description: "Compact foldable smartphone with a stylish design.",
+    colors: [
+      {
+        name: "Cream",
+        image: "/images/galaxy-z-flip3/cream/front.jpg",
+        images: [
+          "/images/galaxy-z-flip3/cream/front.jpg",
+          "/images/galaxy-z-flip3/cream/back.jpg",
+          "/images/galaxy-z-flip3/cream/side.jpg",
+        ],
+      },
+      {
+        name: "Green",
+        image: "/images/galaxy-z-flip3/green/front.jpg",
+        images: [
+          "/images/galaxy-z-flip3/green/front.jpg",
+          "/images/galaxy-z-flip3/green/back.jpg",
+          "/images/galaxy-z-flip3/green/side.jpg",
+        ],
+      },
+      {
+        name: "Lavender",
+        image: "/images/galaxy-z-flip3/lavender/front.jpg",
+        images: [
+          "/images/galaxy-z-flip3/lavender/front.jpg",
+          "/images/galaxy-z-flip3/lavender/back.jpg",
+          "/images/galaxy-z-flip3/lavender/side.jpg",
+        ],
+      },
+      {
+        name: "Phantom Black",
+        image: "/images/galaxy-z-flip3/black/front.jpg",
+        images: [
+          "/images/galaxy-z-flip3/black/front.jpg",
+          "/images/galaxy-z-flip3/black/back.jpg",
+          "/images/galaxy-z-flip3/black/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 999,
+      },
+      {
+        size: "256GB",
+        price: 1049,
+      },
+    ],
+    batteryCapacity: "3300mAh",
+    frontCamera: "10MP",
+    mainCamera: "12MP Wide + 12MP Ultra Wide",
+    cores: 8,
+    screenDiagonal: "6.7 inches",
+    screenResolution: "2640 x 1080 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "426 ppi",
+    screenType: "Dynamic AMOLED 2X",
+    additionally: ["Foldable Display", "Water Resistant (IPX8)", "Flex Mode"],
+    cpu: "Snapdragon 888",
+  },
+  {
+    id: "galaxy-z-flip-4",
+    name: "Samsung Galaxy Z Flip 4",
+    description:
+      "Refined foldable design with improved hinge and better battery life.",
+    colors: [
+      {
+        name: "Graphite",
+        image: "/images/galaxy-z-flip4/graphite/front.jpg",
+        images: [
+          "/images/galaxy-z-flip4/graphite/front.jpg",
+          "/images/galaxy-z-flip4/graphite/back.jpg",
+          "/images/galaxy-z-flip4/graphite/side.jpg",
+        ],
+      },
+      {
+        name: "Bora Purple",
+        image: "/images/galaxy-z-flip4/bora-purple/front.jpg",
+        images: [
+          "/images/galaxy-z-flip4/bora-purple/front.jpg",
+          "/images/galaxy-z-flip4/bora-purple/back.jpg",
+          "/images/galaxy-z-flip4/bora-purple/side.jpg",
+        ],
+      },
+      {
+        name: "Blue",
+        image: "/images/galaxy-z-flip4/blue/front.jpg",
+        images: [
+          "/images/galaxy-z-flip4/blue/front.jpg",
+          "/images/galaxy-z-flip4/blue/back.jpg",
+          "/images/galaxy-z-flip4/blue/side.jpg",
+        ],
+      },
+      {
+        name: "Pink Gold",
+        image: "/images/galaxy-z-flip4/pink-gold/front.jpg",
+        images: [
+          "/images/galaxy-z-flip4/pink-gold/front.jpg",
+          "/images/galaxy-z-flip4/pink-gold/back.jpg",
+          "/images/galaxy-z-flip4/pink-gold/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 999,
+      },
+      {
+        size: "256GB",
+        price: 1059,
+      },
+      {
+        size: "512GB",
+        price: 1179,
+      },
+    ],
+    batteryCapacity: "3700mAh",
+    frontCamera: "10MP",
+    mainCamera: "12MP Wide + 12MP Ultra Wide",
+    cores: 8,
+    screenDiagonal: "6.7 inches",
+    screenResolution: "2640 x 1080 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "426 ppi",
+    screenType: "Dynamic AMOLED 2X",
+    additionally: ["Foldable Display", "Water Resistant (IPX8)", "Flex Mode"],
+    cpu: "Snapdragon 8+ Gen 1",
+  },
+];
