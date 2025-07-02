eeef763f1068f15c8283a79f5da7f30df2592a1c
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-02 09:56:15 -0700

    Added OnePlus 9 product data with color variants, images, and detailed descriptions

---
+++ src/ProductData/Phones/OnePlus/Oneplus.js
@@ -0,0 +1,221 @@
+export const oneplusPhones = [
+  {
+    id: "oneplus-9",
+    name: "OnePlus 9",
+    description:
+      "Flagship-level phone with Hasselblad camera and smooth performance.",
+    colors: [
+      {
+        name: "Winter Mist",
+        image: "/images/oneplus-9/winter-mist/front.jpg",
+        images: [
+          "/images/oneplus-9/winter-mist/front.jpg",
+          "/images/oneplus-9/winter-mist/back.jpg",
+          "/images/oneplus-9/winter-mist/side.jpg",
+        ],
+      },
+      {
+        name: "Arctic Sky",
+        image: "/images/oneplus-9/arctic-sky/front.jpg",
+        images: [
+          "/images/oneplus-9/arctic-sky/front.jpg",
+          "/images/oneplus-9/arctic-sky/back.jpg",
+          "/images/oneplus-9/arctic-sky/side.jpg",
+        ],
+      },
+      {
+        name: "Astral Black",
+        image: "/images/oneplus-9/astral-black/front.jpg",
+        images: [
+          "/images/oneplus-9/astral-black/front.jpg",
+          "/images/oneplus-9/astral-black/back.jpg",
+          "/images/oneplus-9/astral-black/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 729,
+      },
+      {
+        size: "256GB",
+        price: 829,
+      },
+    ],
+    batteryCapacity: "4500mAh",
+    frontCamera: "16MP",
+    mainCamera: "48MP Wide + 50MP Ultra Wide + 2MP Monochrome",
+    cores: 8,
+    screenDiagonal: "6.55 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "402 ppi",
+    screenType: "Fluid AMOLED",
+    additionally: ["65W Fast Charging", "Hasselblad Camera", "5G"],
+    cpu: "Snapdragon 888",
+  },
+  {
+    id: "oneplus-9-pro",
+    name: "OnePlus 9 Pro",
+    description:
+      "High-end OnePlus phone with curved screen and Hasselblad-powered camera.",
+    colors: [
+      {
+        name: "Morning Mist",
+        image: "/images/oneplus-9-pro/morning-mist/front.jpg",
+        images: [
+          "/images/oneplus-9-pro/morning-mist/front.jpg",
+          "/images/oneplus-9-pro/morning-mist/back.jpg",
+          "/images/oneplus-9-pro/morning-mist/side.jpg",
+        ],
+      },
+      {
+        name: "Pine Green",
+        image: "/images/oneplus-9-pro/pine-green/front.jpg",
+        images: [
+          "/images/oneplus-9-pro/pine-green/front.jpg",
+          "/images/oneplus-9-pro/pine-green/back.jpg",
+          "/images/oneplus-9-pro/pine-green/side.jpg",
+        ],
+      },
+      {
+        name: "Stellar Black",
+        image: "/images/oneplus-9-pro/stellar-black/front.jpg",
+        images: [
+          "/images/oneplus-9-pro/stellar-black/front.jpg",
+          "/images/oneplus-9-pro/stellar-black/back.jpg",
+          "/images/oneplus-9-pro/stellar-black/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 969,
+      },
+      {
+        size: "256GB",
+        price: 1069,
+      },
+    ],
+    batteryCapacity: "4500mAh",
+    frontCamera: "16MP",
+    mainCamera: "48MP Wide + 50MP Ultra Wide + 8MP Telephoto + 2MP Monochrome",
+    cores: 8,
+    screenDiagonal: "6.7 inches",
+    screenResolution: "3216 x 1440 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "525 ppi",
+    screenType: "Fluid AMOLED",
+    additionally: [
+      "65W Wired & 50W Wireless Charging",
+      "IP68",
+      "Hasselblad Camera",
+    ],
+    cpu: "Snapdragon 888",
+  },
+  {
+    id: "oneplus-nord-2",
+    name: "OnePlus Nord 2",
+    description:
+      "Mid-range OnePlus phone with AI camera and great performance.",
+    colors: [
+      {
+        name: "Gray Sierra",
+        image: "/images/oneplus-nord-2/gray-sierra/front.jpg",
+        images: [
+          "/images/oneplus-nord-2/gray-sierra/front.jpg",
+          "/images/oneplus-nord-2/gray-sierra/back.jpg",
+          "/images/oneplus-nord-2/gray-sierra/side.jpg",
+        ],
+      },
+      {
+        name: "Blue Haze",
+        image: "/images/oneplus-nord-2/blue-haze/front.jpg",
+        images: [
+          "/images/oneplus-nord-2/blue-haze/front.jpg",
+          "/images/oneplus-nord-2/blue-haze/back.jpg",
+          "/images/oneplus-nord-2/blue-haze/side.jpg",
+        ],
+      },
+      {
+        name: "Green Woods",
+        image: "/images/oneplus-nord-2/green-woods/front.jpg",
+        images: [
+          "/images/oneplus-nord-2/green-woods/front.jpg",
+          "/images/oneplus-nord-2/green-woods/back.jpg",
+          "/images/oneplus-nord-2/green-woods/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 399,
+      },
+      {
+        size: "256GB",
+        price: 499,
+      },
+    ],
+    batteryCapacity: "4500mAh",
+    frontCamera: "32MP",
+    mainCamera: "50MP Wide + 8MP Ultra Wide + 2MP Monochrome",
+    cores: 8,
+    screenDiagonal: "6.43 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "90Hz",
+    pixelDensity: "410 ppi",
+    screenType: "Fluid AMOLED",
+    additionally: ["65W Fast Charging", "AI Photo Enhancement", "Dual SIM"],
+    cpu: "MediaTek Dimensity 1200-AI",
+  },
+  {
+    id: "oneplus-10-pro",
+    name: "OnePlus 10 Pro",
+    description:
+      "Flagship 2022 OnePlus with LTPO 2.0 screen and powerful Snapdragon chip.",
+    colors: [
+      {
+        name: "Volcanic Black",
+        image: "/images/oneplus-10-pro/black/front.jpg",
+        images: [
+          "/images/oneplus-10-pro/black/front.jpg",
+          "/images/oneplus-10-pro/black/back.jpg",
+          "/images/oneplus-10-pro/black/side.jpg",
+        ],
+      },
+      {
+        name: "Emerald Forest",
+        image: "/images/oneplus-10-pro/green/front.jpg",
+        images: [
+          "/images/oneplus-10-pro/green/front.jpg",
+          "/images/oneplus-10-pro/green/back.jpg",
+          "/images/oneplus-10-pro/green/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 899,
+      },
+      {
+        size: "256GB",
+        price: 969,
+      },
+    ],
+    batteryCapacity: "5000mAh",
+    frontCamera: "32MP",
+    mainCamera: "48MP Wide + 50MP Ultra Wide + 8MP Telephoto",
+    cores: 8,
+    screenDiagonal: "6.7 inches",
+    screenResolution: "3216 x 1440 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "525 ppi",
+    screenType: "LTPO2 AMOLED",
+    additionally: ["80W SuperVOOC Charging", "Hasselblad Color Calib.", "5G"],
+    cpu: "Snapdragon 8 Gen 1",
+  },
+];
