698bfe3bfc7f23a6b3bb09fe2c0834c24c2dad7d
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-01 23:55:33 -0700

    Add Google Pixel 6 product data with color variants and image paths
    - Created `Pixel.js` file inside `ProductData/Phones/Google/`
    - Added product ID, name, and description for Google Pixel 6
    - Included three color options: Sorta Seafoam, Kinda Coral, and Stormy Black
    - Linked multiple image views (front, back, side) for each color

---
+++ src/ProductData/Phones/Google/Pixel.js
@@ -0,0 +1,219 @@
+export const googlePhones = [
+  {
+    id: "pixel-6",
+    name: "Google Pixel 6",
+    description:
+      "Google's first custom chip phone with a bold design and excellent camera.",
+    colors: [
+      {
+        name: "Sorta Seafoam",
+        image: "/images/pixel-6/sorta-seafoam/front.jpg",
+        images: [
+          "/images/pixel-6/sorta-seafoam/front.jpg",
+          "/images/pixel-6/sorta-seafoam/back.jpg",
+          "/images/pixel-6/sorta-seafoam/side.jpg",
+        ],
+      },
+      {
+        name: "Kinda Coral",
+        image: "/images/pixel-6/kinda-coral/front.jpg",
+        images: [
+          "/images/pixel-6/kinda-coral/front.jpg",
+          "/images/pixel-6/kinda-coral/back.jpg",
+          "/images/pixel-6/kinda-coral/side.jpg",
+        ],
+      },
+      {
+        name: "Stormy Black",
+        image: "/images/pixel-6/stormy-black/front.jpg",
+        images: [
+          "/images/pixel-6/stormy-black/front.jpg",
+          "/images/pixel-6/stormy-black/back.jpg",
+          "/images/pixel-6/stormy-black/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 599,
+      },
+      {
+        size: "256GB",
+        price: 699,
+      },
+    ],
+    batteryCapacity: "4614mAh",
+    frontCamera: "8MP",
+    mainCamera: "50MP Wide + 12MP Ultra Wide",
+    cores: 8,
+    screenDiagonal: "6.4 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "90Hz",
+    pixelDensity: "411 ppi",
+    screenType: "AMOLED",
+    additionally: [
+      "IP68 Water Resistant",
+      "Google Tensor chip",
+      "Titan M2 security",
+    ],
+    cpu: "Google Tensor",
+  },
+  {
+    id: "pixel-6-pro",
+    name: "Google Pixel 6 Pro",
+    description:
+      "Premium Pixel with triple camera, curved display, and Google's custom chip.",
+    colors: [
+      {
+        name: "Stormy Black",
+        image: "/images/pixel-6-pro/stormy-black/front.jpg",
+        images: [
+          "/images/pixel-6-pro/stormy-black/front.jpg",
+          "/images/pixel-6-pro/stormy-black/back.jpg",
+          "/images/pixel-6-pro/stormy-black/side.jpg",
+        ],
+      },
+      {
+        name: "Cloudy White",
+        image: "/images/pixel-6-pro/cloudy-white/front.jpg",
+        images: [
+          "/images/pixel-6-pro/cloudy-white/front.jpg",
+          "/images/pixel-6-pro/cloudy-white/back.jpg",
+          "/images/pixel-6-pro/cloudy-white/side.jpg",
+        ],
+      },
+      {
+        name: "Sorta Sunny",
+        image: "/images/pixel-6-pro/sorta-sunny/front.jpg",
+        images: [
+          "/images/pixel-6-pro/sorta-sunny/front.jpg",
+          "/images/pixel-6-pro/sorta-sunny/back.jpg",
+          "/images/pixel-6-pro/sorta-sunny/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 899,
+      },
+      {
+        size: "256GB",
+        price: 999,
+      },
+      {
+        size: "512GB",
+        price: 1099,
+      },
+    ],
+    batteryCapacity: "5003mAh",
+    frontCamera: "11.1MP",
+    mainCamera: "50MP Wide + 48MP Telephoto + 12MP Ultra Wide",
+    cores: 8,
+    screenDiagonal: "6.7 inches",
+    screenResolution: "3120 x 1440 pixels",
+    refreshRate: "120Hz",
+    pixelDensity: "512 ppi",
+    screenType: "LTPO OLED",
+    additionally: [
+      "IP68 Water Resistant",
+      "Google Tensor chip",
+      "Titan M2 security",
+    ],
+    cpu: "Google Tensor",
+  },
+  {
+    id: "pixel-6a",
+    name: "Google Pixel 6a",
+    description: "Budget Pixel with Tensor chip and excellent photography.",
+    colors: [
+      {
+        name: "Charcoal",
+        image: "/images/pixel-6a/charcoal/front.jpg",
+        images: [
+          "/images/pixel-6a/charcoal/front.jpg",
+          "/images/pixel-6a/charcoal/back.jpg",
+          "/images/pixel-6a/charcoal/side.jpg",
+        ],
+      },
+      {
+        name: "Chalk",
+        image: "/images/pixel-6a/chalk/front.jpg",
+        images: [
+          "/images/pixel-6a/chalk/front.jpg",
+          "/images/pixel-6a/chalk/back.jpg",
+          "/images/pixel-6a/chalk/side.jpg",
+        ],
+      },
+      {
+        name: "Sage",
+        image: "/images/pixel-6a/sage/front.jpg",
+        images: [
+          "/images/pixel-6a/sage/front.jpg",
+          "/images/pixel-6a/sage/back.jpg",
+          "/images/pixel-6a/sage/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 449,
+      },
+    ],
+    batteryCapacity: "4410mAh",
+    frontCamera: "8MP",
+    mainCamera: "12.2MP Wide + 12MP Ultra Wide",
+    cores: 8,
+    screenDiagonal: "6.1 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "60Hz",
+    pixelDensity: "429 ppi",
+    screenType: "OLED",
+    additionally: [
+      "Google Tensor chip",
+      "Titan M2 security",
+      "5G connectivity",
+    ],
+    cpu: "Google Tensor",
+  },
+  {
+    id: "pixel-5a-5g",
+    name: "Google Pixel 5a 5G",
+    description:
+      "Affordable 5G Pixel with long battery life and clean Android experience.",
+    colors: [
+      {
+        name: "Mostly Black",
+        image: "/images/pixel-5a/mostly-black/front.jpg",
+        images: [
+          "/images/pixel-5a/mostly-black/front.jpg",
+          "/images/pixel-5a/mostly-black/back.jpg",
+          "/images/pixel-5a/mostly-black/side.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 449,
+      },
+    ],
+    batteryCapacity: "4680mAh",
+    frontCamera: "8MP",
+    mainCamera: "12.2MP Wide + 16MP Ultra Wide",
+    cores: 8,
+    screenDiagonal: "6.34 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "60Hz",
+    pixelDensity: "415 ppi",
+    screenType: "OLED",
+    additionally: [
+      "IP67 Water Resistant",
+      "Titan M security",
+      "Stereo Speakers",
+    ],
+    cpu: "Snapdragon 765G",
+  },
+];
