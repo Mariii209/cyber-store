dfd874ce69efc0f4a9a1cbfde76744bc727426bd
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-02 09:57:00 -0700

    Added Vivo V23 5G product data with color variants, images, and memory-based pricing

---
+++ src/ProductData/Phones/Vivo/Vivo.js
@@ -0,0 +1,178 @@
+export const vivoPhones = [
+  {
+    id: "vivo-v23-5g",
+    name: "Vivo V23 5G",
+    description: "Stylish phone with color-changing back and great selfies.",
+    colors: [
+      {
+        name: "Sunshine Gold",
+        image: "/images/vivo-v23-5g/sunshine-gold/front.jpg",
+        images: [
+          "/images/vivo-v23-5g/sunshine-gold/front.jpg",
+          "/images/vivo-v23-5g/sunshine-gold/back.jpg",
+        ],
+      },
+      {
+        name: "Stardust Black",
+        image: "/images/vivo-v23-5g/stardust-black/front.jpg",
+        images: [
+          "/images/vivo-v23-5g/stardust-black/front.jpg",
+          "/images/vivo-v23-5g/stardust-black/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 499,
+      },
+      {
+        size: "256GB",
+        price: 599,
+      },
+    ],
+    batteryCapacity: "4200mAh",
+    frontCamera: "50MP + 8MP Portrait",
+    mainCamera: "64MP + 8MP + 2MP",
+    cores: 8,
+    screenDiagonal: "6.44 inches",
+    screenResolution: "2400 x 1080",
+    refreshRate: "90Hz",
+    pixelDensity: "408 ppi",
+    screenType: "AMOLED",
+    additionally: ["Color-changing back", "Dual-View video", "33W FlashCharge"],
+    cpu: "MediaTek Dimensity 920",
+  },
+  {
+    id: "vivo-v23e",
+    name: "Vivo V23e",
+    description: "Slim mid-range selfie phone with stylish design.",
+    colors: [
+      {
+        name: "Moonlight Shadow",
+        image: "/images/vivo-v23e/moonlight-shadow/front.jpg",
+        images: [
+          "/images/vivo-v23e/moonlight-shadow/front.jpg",
+          "/images/vivo-v23e/moonlight-shadow/back.jpg",
+        ],
+      },
+      {
+        name: "Sunshine Coast",
+        image: "/images/vivo-v23e/sunshine-coast/front.jpg",
+        images: [
+          "/images/vivo-v23e/sunshine-coast/front.jpg",
+          "/images/vivo-v23e/sunshine-coast/back.jpg",
+        ],
+      },
+      {
+        name: "Sunshine Gold",
+        image: "/images/vivo-v23e/sunshine-gold/front.jpg",
+        images: [
+          "/images/vivo-v23e/sunshine-gold/front.jpg",
+          "/images/vivo-v23e/sunshine-gold/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 349,
+      },
+    ],
+    batteryCapacity: "4030mAh",
+    frontCamera: "50MP",
+    mainCamera: "64MP + 8MP + 2MP",
+    cores: 8,
+    screenDiagonal: "6.44 inches",
+    screenResolution: "2408 x 1080",
+    refreshRate: "60Hz",
+    pixelDensity: "409 ppi",
+    screenType: "AMOLED",
+    additionally: ["Slim design", "33W FastCharge", "Side fingerprint"],
+    cpu: "MediaTek Helio G96",
+  },
+  {
+    id: "vivo-x80",
+    name: "Vivo X80",
+    description: "Flagship with Zeiss optics and powerful Dimensity 9000.",
+    colors: [
+      {
+        name: "Cosmic Black",
+        image: "/images/vivo-x80/cosmic-black/front.jpg",
+        images: [
+          "/images/vivo-x80/cosmic-black/front.jpg",
+          "/images/vivo-x80/cosmic-black/back.jpg",
+        ],
+      },
+      {
+        name: "Urban Blue",
+        image: "/images/vivo-x80/urban-blue/front.jpg",
+        images: [
+          "/images/vivo-x80/urban-blue/front.jpg",
+          "/images/vivo-x80/urban-blue/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 799,
+      },
+      {
+        size: "256GB",
+        price: 899,
+      },
+    ],
+    batteryCapacity: "4500mAh",
+    frontCamera: "32MP",
+    mainCamera: "50MP + 12MP + 12MP",
+    cores: 8,
+    screenDiagonal: "6.78 inches",
+    screenResolution: "2772 x 1240",
+    refreshRate: "120Hz",
+    pixelDensity: "451 ppi",
+    screenType: "LTPO AMOLED",
+    additionally: ["Zeiss T* coating", "Nocturnal Imaging", "80W FlashCharge"],
+    cpu: "MediaTek Dimensity 9000",
+  },
+  {
+    id: "vivo-y75-5g",
+    name: "Vivo Y75 5G",
+    description: "Affordable 5G phone with slim body and 44W FlashCharge.",
+    colors: [
+      {
+        name: "Starlight Black",
+        image: "/images/vivo-y75-5g/starlight-black/front.jpg",
+        images: [
+          "/images/vivo-y75-5g/starlight-black/front.jpg",
+          "/images/vivo-y75-5g/starlight-black/back.jpg",
+        ],
+      },
+      {
+        name: "Glowing Galaxy",
+        image: "/images/vivo-y75-5g/glowing-galaxy/front.jpg",
+        images: [
+          "/images/vivo-y75-5g/glowing-galaxy/front.jpg",
+          "/images/vivo-y75-5g/glowing-galaxy/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 299,
+      },
+    ],
+    batteryCapacity: "4100mAh",
+    frontCamera: "16MP",
+    mainCamera: "50MP + 2MP + 2MP",
+    cores: 8,
+    screenDiagonal: "6.44 inches",
+    screenResolution: "2400 x 1080",
+    refreshRate: "60Hz",
+    pixelDensity: "409 ppi",
+    screenType: "AMOLED",
+    additionally: ["Slim body", "44W FlashCharge", "Side fingerprint"],
+    cpu: "MediaTek Dimensity 700",
+  },
+];
