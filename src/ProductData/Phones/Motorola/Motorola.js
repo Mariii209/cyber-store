c05f7ed3e0eb74a5b4603ecaa76a1e213e51b77f
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-02 09:55:21 -0700

    Added Motorola Edge 2022 product data with memory options, colors, images, and full specifications

---
+++ src/ProductData/Phones/Motorola/Motorola.js
@@ -0,0 +1,176 @@
+export const motorolaPhones = [
+  {
+    id: "motorola-edge-2022",
+    name: "Motorola Edge 2022",
+    description:
+      "Flagship Motorola with curved OLED display and 144\u202fHz refresh rate.",
+    colors: [
+      {
+        name: "Mineral Gray",
+        image: "/images/motorola-edge-2022/mineral-gray/front.jpg",
+        images: [
+          "/images/motorola-edge-2022/mineral-gray/front.jpg",
+          "/images/motorola-edge-2022/mineral-gray/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "256GB",
+        price: 699,
+      },
+      {
+        size: "512GB",
+        price: 799,
+      },
+    ],
+    batteryCapacity: "5000mAh",
+    frontCamera: "32MP",
+    mainCamera: "50MP Wide + 13MP Ultra Wide + 2MP Depth",
+    cores: 8,
+    screenDiagonal: "6.6 inches",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "144Hz",
+    pixelDensity: "399 ppi",
+    screenType: "OLED",
+    additionally: ["5G", "144Hz Display", "Stereo Speakers"],
+    cpu: "MediaTek Dimensity 1050",
+  },
+  {
+    id: "moto-g-stylus-2023",
+    name: "Moto G Stylus 2023",
+    description: "Mid-range with built-in stylus and 50\u202fMP camera.",
+    colors: [
+      {
+        name: "Glam Pink",
+        image: "/images/moto-g-stylus-2023/glam-pink/front.jpg",
+        images: [
+          "/images/moto-g-stylus-2023/glam-pink/front.jpg",
+          "/images/moto-g-stylus-2023/glam-pink/back.jpg",
+        ],
+      },
+      {
+        name: "Midnight Blue",
+        image: "/images/moto-g-stylus-2023/midnight-blue/front.jpg",
+        images: [
+          "/images/moto-g-stylus-2023/midnight-blue/front.jpg",
+          "/images/moto-g-stylus-2023/midnight-blue/back.jpg",
+        ],
+      },
+      {
+        name: "Deep Indigo",
+        image: "/images/moto-g-stylus-2023/deep-indigo/front.jpg",
+        images: [
+          "/images/moto-g-stylus-2023/deep-indigo/front.jpg",
+          "/images/moto-g-stylus-2023/deep-indigo/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "128GB",
+        price: 199,
+      },
+      {
+        size: "256GB",
+        price: 249,
+      },
+    ],
+    batteryCapacity: "5000mAh",
+    frontCamera: "16MP",
+    mainCamera: "50MP Wide + 2MP Macro",
+    cores: 8,
+    screenDiagonal: "6.5 inches",
+    screenResolution: "1600 x 720 pixels",
+    refreshRate: "90Hz",
+    pixelDensity: "270 ppi",
+    screenType: "IPS LCD",
+    additionally: ["Stylus", "Headphone Jack", "Dual SIM"],
+    cpu: "MediaTek Helio G85",
+  },
+  {
+    id: "motorola-razr-2022",
+    name: "Motorola Razr (2022)",
+    description:
+      "Foldable design with top-tier performance and external screen.",
+    colors: [
+      {
+        name: "Satin Black",
+        image: "/images/motorola-razr-2022/satin-black/front.jpg",
+        images: [
+          "/images/motorola-razr-2022/satin-black/front.jpg",
+          "/images/motorola-razr-2022/satin-black/back.jpg",
+        ],
+      },
+      {
+        name: "Polished Graphite",
+        image: "/images/motorola-razr-2022/polished-graphite/front.jpg",
+        images: [
+          "/images/motorola-razr-2022/polished-graphite/front.jpg",
+          "/images/motorola-razr-2022/polished-graphite/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "256GB",
+        price: 999,
+      },
+    ],
+    batteryCapacity: "3500mAh",
+    frontCamera: "32MP",
+    mainCamera: "50MP Wide + 13MP Ultra Wide",
+    cores: 8,
+    screenDiagonal: "6.7 inches (main), 2.7 inches (cover)",
+    screenResolution: "2400 x 1080 pixels",
+    refreshRate: "144Hz",
+    pixelDensity: "393 ppi",
+    screenType: "Foldable AMOLED",
+    additionally: ["Foldable", "External Display", "5G"],
+    cpu: "Snapdragon 8+ Gen 1",
+  },
+  {
+    id: "moto-g-power-2022",
+    name: "Moto G Power (2022)",
+    description: "Long battery life and water-repellent design.",
+    colors: [
+      {
+        name: "Ice Blue",
+        image: "/images/moto-g-power-2022/ice-blue/front.jpg",
+        images: [
+          "/images/moto-g-power-2022/ice-blue/front.jpg",
+          "/images/moto-g-power-2022/ice-blue/back.jpg",
+        ],
+      },
+      {
+        name: "Dark Grove",
+        image: "/images/moto-g-power-2022/dark-grove/front.jpg",
+        images: [
+          "/images/moto-g-power-2022/dark-grove/front.jpg",
+          "/images/moto-g-power-2022/dark-grove/back.jpg",
+        ],
+      },
+    ],
+    memoryOptions: [
+      {
+        size: "64GB",
+        price: 169,
+      },
+      {
+        size: "128GB",
+        price: 199,
+      },
+    ],
+    batteryCapacity: "5000mAh",
+    frontCamera: "8MP",
+    mainCamera: "50MP Wide + 2MP Depth + 2MP Macro",
+    cores: 8,
+    screenDiagonal: "6.5 inches",
+    screenResolution: "1600 x 720 pixels",
+    refreshRate: "90Hz",
+    pixelDensity: "270 ppi",
+    screenType: "IPS LCD",
+    additionally: ["Water-Repellent", "MicroSD Slot", "3-day Battery"],
+    cpu: "MediaTek Helio G37",
+  },
+];
