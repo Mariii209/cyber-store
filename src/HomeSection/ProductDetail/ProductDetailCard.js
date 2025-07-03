574d8f30b4084db91c9d8d5141c8e4c9e10e9fa4
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-03 13:39:01 -0700

    Add ProductDetailCard.css component with styling for product detail view

--- src/HomeSection/ProductDetail/ProductDetailCard.js
+++ src/HomeSection/ProductDetail/ProductDetailCard.js
@@ -1,78 +1,85 @@
 import React from "react";
+import "./ProductDetailCard.css";
 
 export default function ProductDetailCard({ product }) {
   return (
     <div className="ProductDetailCard">
-      <img src={product.image} alt={product.name} />
-      <h1>Apple iPhone 14 Pro Max</h1>
-      <h4>$1399</h4>
-      <h5>$1499</h5>
-      <p>Select color :</p>
-      <div className="ColorOptions">
-        <div
-          className="ColorOption"
-          style={{ backgroundColor: "#000000" }}
-        ></div>
-        <div
-          className="ColorOption"
-          style={{ backgroundColor: "#FFFFFF" }}
-        ></div>
-        <div
-          className="ColorOption"
-          style={{ backgroundColor: "#FF0000" }}
-        ></div>
-        <div>
-          <i class="fa-solid fa-mobile-screen-button"></i>
-          <div>
-            <p>Screen size</p>
-            <p>6.7"</p>
-          </div>
-        </div>
-        <div>
-          <i class="fa-solid fa-microchip"></i>
-          <div>
-            <p>Screen size</p>
-            <p>6.7"</p>
-          </div>
-        </div>
-        <div>
-          <i class="fas fa-microchip"></i>
+      <img className="ProductImage" src={product.image} alt={product.name} />
 
-          <div>
-            <p>Screen size</p>
-            <p>6.7"</p>
-          </div>
-        </div>
-        <div>
-          <i class="fa-solid fa-camera"></i>
-          <div>
-            <p>Screen size</p>
-            <p>6.7"</p>
-          </div>
-        </div>
-        <div>
-          <i class="fa-solid fa-camera-rotate"></i>
-          <div>
-            <p>Screen size</p>
-            <p>6.7"</p>
-          </div>
-        </div>
-        <div>
-          <i class="fa-solid fa-battery-three-quarters"></i>
-          <div>
-            <p>Screen size</p>
-            <p>6.7"</p>
-          </div>
-        </div>
-        <p>
-          Enhanced capabilities thanks toan enlarged display of 6.7 inchesand
-          work without rechargingthroughout the day. Incredible photosas in
-          weak, yesand in bright lightusing the new systemwith two cameras
-          more...
-        </p>
+      <h1 className="ProductTitle">Apple iPhone 14 Pro Max</h1>
+      <div className="PriceContainer">
+        <h4 className="ProductPrice">$1399</h4>
+        <h5 className="OriginalPrice">$1499</h5>
+      </div>
 
-        <button>Add to Wishlist</button>
-        <button>Add to Cart</button>
+      <div className="ColorContainer">
+        <p className="ColorLabel">Select color:</p>
+        <div className="ColorOptions">
+          <div className="ColorOption"></div>
+          <div className="ColorOption"></div>
+          <div className="ColorOption"></div>
+        </div>
+      </div>
+
+      <div className="ProductSpecs">
+        <div className="ProductSpec">
+          <i className="fa-solid fa-mobile-screen-button SpecIcon"></i>
+          <div className="SpecDetail">
+            <p className="SpecLabel">Screen Size</p>
+            <p className="SpecValue">6.7"</p>
+          </div>
+        </div>
+
+        <div className="ProductSpec">
+          <i className="fa-solid fa-microchip SpecIcon"></i>
+          <div className="SpecDetail">
+            <p className="SpecLabel">CPU</p>
+            <p className="SpecValue">Apple A16 Bionic</p>
+          </div>
+        </div>
+
+        <div className="ProductSpec">
+          <i className="fa-solid fa-microchip SpecIcon"></i>
+          <div className="SpecDetail">
+            <p className="SpecLabel">Number of Cores</p>
+            <p className="SpecValue">6</p>
+          </div>
+        </div>
+
+        <div className="ProductSpec">
+          <i className="fa-solid fa-camera SpecIcon"></i>
+          <div className="SpecDetail">
+            <p className="SpecLabel">Main Camera</p>
+            <p className="SpecValue">48 MP</p>
+          </div>
+        </div>
+
+        <div className="ProductSpec">
+          <i className="fa-solid fa-camera-rotate SpecIcon"></i>
+          <div className="SpecDetail">
+            <p className="SpecLabel">Front Camera</p>
+            <p className="SpecValue">12 MP</p>
+          </div>
+        </div>
+
+        <div className="ProductSpec">
+          <i className="fa-solid fa-battery-three-quarters SpecIcon"></i>
+          <div className="SpecDetail">
+            <p className="SpecLabel">Battery Life</p>
+            <p className="SpecValue">Up to 29 hrs</p>
+          </div>
+        </div>
+      </div>
+
+      <p className="ProductDescription">
+        Enhanced capabilities thanks to an enlarged display of 6.7 inches and
+        all-day battery life. Capture incredible photos in low or bright light
+        with a powerful new dual-camera system.
+      </p>
+
+      <div className="ProductActions">
+        <button className="WishlistButton">Add to Wishlist</button>
+        <button className="CartButton">Add to Cart</button>
       </div>
     </div>
   );
